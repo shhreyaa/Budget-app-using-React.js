e29dc8ab14864cb0af16b7d50094388ff22b69e7
Shreya Basnet <basnet.shreyaa@gmail.com>
2021-05-31 22:45:07 +0545

    form update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Expenses from "./components/Expenses";
-
-function App() {
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from './components/NewExpense/NewExpense'
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -25,10 +25,10 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
