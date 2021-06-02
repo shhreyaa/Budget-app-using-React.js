a7669547f04462e06e85ec05ad3d7628d0adaca9
Shreya Basnet <basnet.shreyaa@gmail.com>
2021-06-02 23:24:37 +0545

    4th module

--- src/App.js
+++ src/App.js
@@ -22,10 +22,13 @@ const App = () => {
       date: new Date(2021, 5, 12),
     },
   ];
-
+const addExpenseHandler = (expense) =>{
+  console.log('in app');
+  console.log(expense);
+}
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense= {addExpenseHandler}/>
       <Expenses items={expenses} />
     </div>
   );
